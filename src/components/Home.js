acb46fbae9c827fc6e5a19726b09d7c8a0cf13fb
Ronak Raizada <raizadaronak18@gmail.com>
2021-03-28 19:18:00 +0530

    connected with repo

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,8 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import PropTypes from 'prop-types';
 import { makeStyles, useTheme } from '@material-ui/core/styles';
 import Table from '@material-ui/core/Table';
+import TableHead from '@material-ui/core/TableHead';
 import TableBody from '@material-ui/core/TableBody';
 import TableCell from '@material-ui/core/TableCell';
 import TableContainer from '@material-ui/core/TableContainer';
@@ -16,6 +17,8 @@ import KeyboardArrowRight from '@material-ui/icons/KeyboardArrowRight';
 import LastPageIcon from '@material-ui/icons/LastPage';
 import Container from '@material-ui/core/Container';
 import MainAppBar from './AppBar'
+
+import { AuthContext } from '../store/Auth';
 const useStyles1 = makeStyles((theme) => ({
   root: {
     flexShrink: 0,
@@ -81,25 +84,6 @@ TablePaginationActions.propTypes = {
   rowsPerPage: PropTypes.number.isRequired,
 };
 
-function createData(name, calories, fat) {
-  return { name, calories, fat };
-}
-
-const rows = [
-  createData('Cupcake', 305, 3.7),
-  createData('Donut', 452, 25.0),
-  createData('Eclair', 262, 16.0),
-  createData('Frozen yoghurt', 159, 6.0),
-  createData('Gingerbread', 356, 16.0),
-  createData('Honeycomb', 408, 3.2),
-  createData('Ice cream sandwich', 237, 9.0),
-  createData('Jelly Bean', 375, 0.0),
-  createData('KitKat', 518, 26.0),
-  createData('Lollipop', 392, 0.2),
-  createData('Marshmallow', 318, 0),
-  createData('Nougat', 360, 19.0),
-  createData('Oreo', 437, 18.0),
-].sort((a, b) => (a.calories < b.calories ? -1 : 1));
 
 const useStyles2 = makeStyles({
   table: {
@@ -108,11 +92,14 @@ const useStyles2 = makeStyles({
 });
 
 export default function Home() {
+  const repos = useContext(AuthContext).repos;
+  var moment = require('moment'); // require
+
   const classes = useStyles2();
   const [page, setPage] = React.useState(0);
   const [rowsPerPage, setRowsPerPage] = React.useState(5);
 
-  const emptyRows = rowsPerPage - Math.min(rowsPerPage, rows.length - page * rowsPerPage);
+  const emptyRows = rowsPerPage - Math.min(rowsPerPage, repos.length - page * rowsPerPage);
 
   const handleChangePage = (event, newPage) => {
     setPage(newPage);
@@ -123,26 +110,62 @@ export default function Home() {
     setPage(0);
   };
 
+
+
+
+
+
   return (
     <React.Fragment>
       <MainAppBar />
       <Container style={{ marginTop: '20px' }}>
         <TableContainer component={Paper}>
           <Table className={classes.table} aria-label="custom pagination table">
+            <TableHead>
+              <TableRow>
+                <TableCell component="th" scope="row">
+                  Name
+                  </TableCell>
+                <TableCell style={{ width: 160 }} align="right">
+                  Description
+                  </TableCell>
+                <TableCell style={{ width: 160 }} align="right">
+                  Collaborators
+                  </TableCell>
+                <TableCell style={{ width: 160 }} align="right">
+                  Link to Github Repo
+                  </TableCell>
+                <TableCell style={{ width: 160 }} align="right">
+                  Created Date
+                  </TableCell>
+                <TableCell style={{ width: 160 }} align="right">
+                  Link to issues page
+                  </TableCell>
+              </TableRow>
+            </TableHead>
             <TableBody>
               {(rowsPerPage > 0
-                ? rows.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
-                : rows
+                ? repos.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+                : repos
               ).map((row) => (
                 <TableRow key={row.name}>
                   <TableCell component="th" scope="row">
                     {row.name}
                   </TableCell>
                   <TableCell style={{ width: 160 }} align="right">
-                    {row.calories}
+                    {row.description}
                   </TableCell>
                   <TableCell style={{ width: 160 }} align="right">
-                    {row.fat}
+                    {row.collaborators_url}
+                  </TableCell>
+                  <TableCell style={{ width: 160 }} align="right">
+                    {row.clone_url}
+                  </TableCell>
+                  <TableCell style={{ width: 160 }} align="right">
+                    {moment(row.created_at).format("DD-MMM-YYYY")}
+                  </TableCell>
+                  <TableCell style={{ width: 160 }} align="right">
+                    {row.issues_url}
                   </TableCell>
                 </TableRow>
               ))}
@@ -158,7 +181,7 @@ export default function Home() {
                 <TablePagination
                   rowsPerPageOptions={[5, 10, 25, { label: 'All', value: -1 }]}
                   colSpan={3}
-                  count={rows.length}
+                  count={repos.length}
                   rowsPerPage={rowsPerPage}
                   page={page}
                   SelectProps={{
--- src/store/Auth.js
+++ src/store/Auth.js
@@ -2,51 +2,58 @@
 import React, { useState } from 'react';
 export const AuthContext = React.createContext({
     isLogin: false,
+    token: '',
+    repos: [],
     login: () => { },
     logout: () => { },
 })
 
 export default props => {
     const [signIn, setSignIn] = useState(false);
-    const loginHandeler = (cb) => {
-        var axios = require('axios');
+    const [accessToken] = useState('3b6649047e32d6e9075cb7a4818d3cdebb43e4a9');
+    const [repositories, setRepositories] = useState([]);
 
-        var config = {
-            method: 'get',
-            url: 'https://github.com/login/oauth/authorize?client_id=Iv1.c2d5e5e8132183d8',
-            headers: {
-                "Access-Control-Allow-Origin":"*"
+    const getRepositories = async () => {
+        const { Octokit } = require("@octokit/core");
+        const octokit = new Octokit({ auth: accessToken });
+        const response = await octokit.request('GET /user')
+        if (response.status === 200) {
+            const user = response.data.login;
+            const repoResponse = await octokit.request(`GET /users/${user}/repos`);
+            if (repoResponse.status === 200) {
+                return repoResponse.data
             }
-        };
+        }
 
-        axios(config)
-            .then(function (response) {
-                console.log(JSON.stringify(response.data));
-            })
-            .catch(function (error) {
-                console.log(error);
-            });
+    }
 
-        const token = 'abcd'
-        localStorage.setItem('token', token);
+    const setRepositoryData = async()=>{
+        const data = await getRepositories()
+        setRepositories(data)
+    }
 
-        if ("token" in localStorage) {
+    const loginHandeler = (cb) => {
+        localStorage.setItem('token',accessToken)
+        if ('token' in localStorage) {
             setSignIn(true)
             cb()
+            setRepositoryData()
         }
 
     }
 
     const logOutHandeler = (cb) => {
-        localStorage.removeItem('token')
         setSignIn(false)
+        localStorage.removeItem('token')
         cb()
     }
 
     return <AuthContext.Provider value={{
         isLogin: signIn,
+        token: accessToken,
+        repos: repositories,
         login: loginHandeler,
-        logout: logOutHandeler
+        logout: logOutHandeler,
     }}>
         {props.children}
 
